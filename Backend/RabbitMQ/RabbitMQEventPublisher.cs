d71565fb6860d8d58d057b7607b7290a2d02e08d
Артур Григорян <arturgrigoryn21@gmail.com>
2026-07-28 22:06:14 +0300

    fix

--- Backend/RabbitMQ/RabbitMQEventPublisher.cs
+++ Backend/RabbitMQ/RabbitMQEventPublisher.cs
@@ -5,6 +5,7 @@ using RabbitMQ.Client.Events;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Channels;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Backend.RabbitMQ;
 
@@ -18,7 +19,6 @@ public class RabbitMQEventPublisher : IEventPublisher, IAsyncDisposable
     private readonly string _exchangeName;
     private readonly string _routingKey;
     private readonly string _hostName;
-    private readonly string _stuckQueue = "stuck_messages_queue";
     private readonly int _port;
     private readonly ILogger<RabbitMQEventPublisher> _logger;
     private readonly SemaphoreSlim _semaphoreSlim = new(1, 1);
@@ -62,7 +62,6 @@ public class RabbitMQEventPublisher : IEventPublisher, IAsyncDisposable
     publisherConfirmationsEnabled: true,
     publisherConfirmationTrackingEnabled: true);
             _channel = await _connection.CreateChannelAsync(channelOpts);
-
             _channel.BasicAcksAsync += BasicAcksAsyncHandler;
             _channel.BasicNacksAsync += BasicNacksAsyncHandler;
             _channel.BasicReturnAsync += BasicReturnAsyncHandler;
@@ -80,12 +79,6 @@ public class RabbitMQEventPublisher : IEventPublisher, IAsyncDisposable
     { "x-dead-letter-exchange", "" }, // Пустая строка = default exchange
     { "x-dead-letter-routing-key", "dead_letter_queue" } // Куда отправлять
 };
-            await _channel.QueueDeclareAsync(
-    queue: _stuckQueue,
-    durable: true,
-    exclusive: false,
-    autoDelete: false
-);
             await _channel.QueueDeclareAsync(
                 queue: queueName,
                 durable: true,
@@ -143,7 +136,7 @@ public class RabbitMQEventPublisher : IEventPublisher, IAsyncDisposable
             //await _channel.BasicQosAsync(0, 10, false);
             await _channel.BasicPublishAsync(
                 exchange: _exchangeName,
-                routingKey: "3e3e3",
+                routingKey: _routingKey,
                 mandatory: true,
                 basicProperties: properties,
                 body: body);
@@ -155,30 +148,8 @@ public class RabbitMQEventPublisher : IEventPublisher, IAsyncDisposable
         catch (Exception ex)
         {
             _logger.LogError(ex, "Failed to publish order status change event for Order ID: {OrderId}", order.Id);
-            await SaveToStuckQueueAsync(body, ex.Message);
             throw;
         }
-    }
-    private async Task SaveToStuckQueueAsync(byte[] body, string reason)
-    {
-        var stuckProps = new BasicProperties
-        {
-            Persistent = true,
-            Headers = new Dictionary<string, object?>
-            {
-                { "x-return-reason", reason },
-                { "x-return-time", DateTime.UtcNow.ToString("o") },
-                { "x-error-type", "producer_return" }
-            }
-        };
-
-        await _channel.BasicPublishAsync(
-            exchange: "",
-            routingKey: _stuckQueue,
-            mandatory: false,
-            basicProperties: stuckProps,
-            body: body
-        );
     }
 
 
@@ -205,7 +176,6 @@ public class RabbitMQEventPublisher : IEventPublisher, IAsyncDisposable
         _logger.LogWarning(
     "Message returned: Exchange={Exchange}, RoutingKey={RoutingKey}, ReplyText={ReplyText}, Body={Body}",
     e.Exchange, e.RoutingKey, e.ReplyText, message);
-        await SaveToStuckQueueAsync(body, e.ReplyText);
     }
 
     /// <summary>
